0d79ec17fb611b80d7cea470f0ff9dd9c915cb54
tuan <tuanigboss1@gmail.com>
2021-03-09 23:37:30 +0700

    Add new file

---
+++ game2048_2/loadTexture.h
@@ -0,0 +1,163 @@
+﻿/*
+Làm theo tutorial tại:
+http://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-2-dont-put-everything-in-main
+*/
+#include <iostream>
+#include <SDL.h>
+//#include <SDL2/SDL.h>
+#include <string>
+using namespace std;
+
+//**************************************************************
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "Load Bitmap Image in SDL";
+
+void logSDLError(std::ostream& os,
+	const std::string &msg, bool fatal = false);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void waitUntilKeyPressed();
+//**************************************************************
+
+// Hai hàm về texture, lấy nguyên về từtutorial tại:
+// http://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-2-dont-put-everything-in-main
+
+// Hàm nạp texture từ file ảnh, để vẽ lên renderer tương ứng
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren);
+
+// Hàm đưa texture ra renderer (để hiển thị ra màn hình) tại toạ độ (x,y) và giữ nguyên kích cỡ ban đầu của ảnh
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
+
+// Hàm (cùng trên nhưng khác tham số với hàm ngay trên)
+// đưa texture ra renderer (để hiển thị ra màn hình) tại toạ độ (x,y) và
+// với kích cỡ rộng (w) và cao (h) cho trước.
+// Ảnh sẽ được co dãn (phóng to thu nhỏ) cho khớp kích cỡ này.
+// Đây là hàm không có trong tutorial ban đầu, được sửa từ hàm trong tutorial.
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h);
+
+
+/**
+* Nạp một ảnh bitmap (BMP) vào một texture trên thiết bị hiện thị (rendering device)
+* @param file: đường dẫn và tên file ảnh BMP
+* @param ren: renderer để nạp texture lên
+* @return trả về texture đã nạp, hoặc nullptr nếu có lỗi.
+*/
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
+{
+	//Khởi tạo là nullptr để tránh lỗi 'dangling pointer'
+	SDL_Texture *texture = nullptr;
+	//Nạp ảnh từ tên file (với đường dẫn)
+	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
+	//Nếu không có lỗi, chuyển đổi về dạng texture and và trả về
+	if (loadedImage != nullptr) {
+		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+		SDL_FreeSurface(loadedImage);
+		//Đảm bảo việc chuyển đổi không có lỗi
+		if (texture == nullptr) {
+			logSDLError(std::cout, "CreateTextureFromSurface");
+		}
+	}
+	else {
+		logSDLError(std::cout, "LoadBMP");
+	}
+	return texture;
+}
+
+/**
+* Vẽ một SDL_Texture lên một SDL_Renderer tại toạ độ (x, y), trong khi
+* giữ nguyên chiều rộng và cao của ảnh
+* @param tex: texture nguồn chúng ta muốn vẽ ra
+* @param ren: thiết bị renderer chúng ta muốn vẽ vào
+* @param x: hoành độ
+* @param y: tung độ
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
+{
+	//Thiết lập hình chữ nhật đích mà chúng ta muốn vẽ ảnh vào trong
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	//Truy vẫn texture để lấy chiều rộng và cao (vào chiều rộng và cao tương ứng của hình chữ nhật đích)
+	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	//Đưa toàn bộ ảnh trong texture vào hình chữ nhật đích
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+/**
+* Vẽ một SDL_Texture lên một SDL_Renderer tại toạ độ (x, y), với
+* chiều rộng và cao mới
+* @param tex: texture nguồn chúng ta muốn vẽ ra
+* @param ren: thiết bị renderer chúng ta muốn vẽ vào
+* @param x: hoành độ
+* @param y: tung độ
+* @param w: chiều rộng (mới)
+* @param h: độ cao (mới)
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h)
+{
+	//Thiết lập hình chữ nhật đích mà chúng ta muốn vẽ ảnh vào trong
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	dst.w = w;
+	dst.h = h;
+	//Đưa toàn bộ ảnh trong texture vào hình chữ nhật đích
+	//(ảnh sẽ co dãn cho khớp với kích cỡ mới)
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+//*****************************************************
+// Các hàm chung về khởi tạo và huỷ SDL
+void logSDLError(std::ostream& os,
+	const std::string &msg, bool fatal)
+{
+	os << msg << " Error: " << SDL_GetError() << std::endl;
+	if (fatal) {
+		SDL_Quit();
+		exit(1);
+	}
+}
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		logSDLError(std::cout, "SDL_Init", true);
+
+	window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	//window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+	//   SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+	if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+
+	//Khi thông thường chạy với môi trường bình thường ở nhà
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+		SDL_RENDERER_PRESENTVSYNC);
+	//Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
+	//renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+	if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void waitUntilKeyPressed()
+{
+	SDL_Event e;
+	while (true) {
+		if (SDL_WaitEvent(&e) != 0 &&
+			(e.type == SDL_KEYDOWN || e.type == SDL_QUIT))
+			return;
+		SDL_Delay(100);
+	}
+}
+//**************************************************************
+
---
+++ game2048_2/main.cpp
@@ -0,0 +1,205 @@
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include "Block.h"
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+void loadBG(SDL_Renderer *renderer) {
+	SDL_Texture *texture = loadTexture("img\\bg.bmp", renderer);
+	renderTexture(texture, renderer, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_DestroyTexture(texture);
+}
+
+
+
+Block GameMain[4][4];
+Block Random[2] = { Block(2),Block(4) };
+const int xp = 25;
+const int yp = SCREEN_HEIGHT / 3;
+
+
+void DrawMainGame(Block GameMain[4][4], SDL_Renderer *renderer);
+void add_new(Block GameMain[4][4]);
+void showGameMain(Block GameMain[4][4], SDL_Renderer *renderer);
+bool test_oke(Block GameMain[4][4]);
+bool RightToLeft(Block GameMain[4][4], long long &points);
+bool LeftToRight(Block GameMain[4][4], long long &points);
+bool UpToDown(Block GameMain[4][4], long long &points);
+bool DownToUp(Block Gamemain[4][4], long long &points);
+
+
+int main(int argc, char* argv[]) {
+	TTF_Init();
+	srand(time(0));
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	initSDL(window, renderer);
+	SDL_Event e;
+	bool running = true;
+	bool right = false;
+	bool left = false;
+	bool up = false;
+	bool down = false;
+	int x = 25;
+	int y = SCREEN_HEIGHT / 3;
+	long long points = 0;
+	loadBG(renderer);
+	DrawMainGame(GameMain, renderer);
+	while (running) {
+		SDL_Delay(10);
+		showGameMain(GameMain, renderer);
+		if (test_oke(GameMain)==false) {
+			running = false;
+			break;
+		}
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				running = false;
+				break;
+			}
+			if (e.type == SDL_KEYDOWN) {
+				if (e.key.keysym.sym == SDLK_ESCAPE) running = false;
+				if (e.key.keysym.sym == SDLK_a) left = true;
+				if (e.key.keysym.sym == SDLK_w) up = true;
+				if (e.key.keysym.sym == SDLK_d) right = true;
+				if (e.key.keysym.sym == SDLK_s) down = true;
+				break;
+			}
+		}
+		if (right) {
+			if (LeftToRight(GameMain, points)==true) add_new(GameMain);
+			right = false;
+		}
+		if (left) {
+			if (RightToLeft(GameMain, points) == true) add_new(GameMain);
+			left = false;
+		}
+		SDL_RenderPresent(renderer);
+	}
+
+	quitSDL(window, renderer);
+	return 0;
+}
+
+
+void DrawMainGame(Block GameMain[4][4], SDL_Renderer *renderer) {
+	for (int cols = 0; cols < 4; cols++) {
+		for (int rows = 0; rows < 4; rows++) {
+			GameMain[cols][rows] = Block(0);
+		}
+	}
+	for (int index = 0; index < 2; index++) {
+		int a = rand() % 4;
+		int b = rand() % 4;
+		if (GameMain[a][b].get_value() == 0) {
+			GameMain[a][b] = Random[rand() % 2];
+		}
+		else {
+			index--;
+		}
+	}
+}
+void showGameMain(Block GameMain[4][4],SDL_Renderer *renderer) {
+	for (int cols = 0; cols < 4; cols++) {
+		for (int rows = 0; rows < 4; rows++) {
+			GameMain[cols][rows].showBlock(xp + 90 * rows, yp + 90 * cols, renderer);
+		}
+	}
+}
+void add_new(Block GameMain[4][4]) {
+	bool oke = true;
+	while (oke) {
+		int a = rand() % 4;
+		int b = rand() % 4;
+		if (GameMain[a][b].get_value() == 0) {
+			GameMain[a][b] = Random[rand() % 2];
+			oke = false;
+		}
+	}
+}
+bool test_oke(Block GameMain[4][4]) {
+	for (int cols = 0; cols < 4; cols++) {
+		for(int rows=0;rows<4;rows++){
+			if (GameMain[cols][rows].get_value() == 0) return true;
+		}
+	}
+	for (int cols = 0; cols < 4; cols++) {
+		for (int rows = 0; rows < 3; rows++) {
+			if (GameMain[cols][rows].get_value() == GameMain[cols][rows + 1].get_value()) return true;
+		}
+	}
+	for (int rows = 0; rows < 4; rows++) {
+		for (int cols = 0; cols < 3; cols++) {
+			if (GameMain[cols][rows].get_value() == GameMain[cols + 1][rows].get_value()) return true;
+		}
+	}
+	return false;
+}
+bool LeftToRight(Block GameMain[4][4], long long &points) {
+	bool success = false;
+	for (short cols = 0; cols < 4; cols++) {
+		for (short rows = 3; rows > 0; rows--) {
+			if (GameMain[cols][rows].get_value() != 0) {
+				for (short index = rows - 1; index >= 0; index--) {
+					if (GameMain[cols][rows] == GameMain[cols][index]) {
+						GameMain[cols][rows] = GameMain[cols][rows] + GameMain[cols][index];
+						points += GameMain[cols][index].get_value();
+						success = true;
+						break;
+					}
+					if (GameMain[cols][rows].get_value() != GameMain[cols][index].get_value() && GameMain[cols][index].get_value() != 0) break;
+				}
+			}
+		}
+	}
+	for (short cols = 0; cols < 4; cols++) {
+		for (short rows = 3; rows > 0; rows--) {
+			if (GameMain[cols][rows].get_value() == 0) {
+				for (short index = rows - 1; index >= 0; index--) {
+					if (GameMain[cols][index].get_value() != 0) {
+						success = true;
+						GameMain[cols][rows] = GameMain[cols][index];
+						GameMain[cols][index] = Block(0);
+						break;
+					}
+				}
+			}
+		}
+	}
+	return success;
+}
+bool RightToLeft(Block GameMain[4][4], long long &points) {
+	bool success = false;
+	for (short cols = 0; cols < 4; cols++) {
+		for (short rows = 0; rows <3 ; rows++) {
+			if (GameMain[cols][rows].get_value() != 0) {
+				for (short index = rows + 1; rows < 4; rows++) {
+					if (GameMain[cols][rows] == GameMain[cols][index]) {
+						GameMain[cols][rows] = GameMain[cols][rows] + GameMain[cols][index];
+						points += GameMain[cols][rows].get_value();
+						success = true;
+						break;
+					}
+					if (GameMain[cols][rows].get_value() != GameMain[cols][index].get_value() && GameMain[cols][rows].get_value() != 0) break;
+				}
+			}
+		}
+	}
+	for (short cols = 0; cols < 4; cols++) {
+		for (short rows = 0; rows< 3; rows++) {
+			if (GameMain[cols][rows].get_value() != 0) {
+				for (short index = rows + 1; rows < 4; rows++) {
+					if (GameMain[cols][index].get_value() != 0) {
+						GameMain[cols][rows] = GameMain[cols][index];
+						GameMain[cols][index] = Block(0);
+						success = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+	return success;
+}
